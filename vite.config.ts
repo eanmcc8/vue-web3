5b46a815f1005288bea81c16f048cca6a76fde76
eanmcc8 <eanmcc86@proton.me>
2026-07-05 13:02:02 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
+import { fileURLToPath, URL } from 'node:url'
+
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  base: '/vue-web3/',
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      // @ts-ignore
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      lib: fileURLToPath(new URL('./lib', import.meta.url))
+    }
+  },
+  build: {
+    outDir: resolve(__dirname, './demo'),
+    emptyOutDir: true
+  }
 })
